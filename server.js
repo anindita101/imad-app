b93441f2edde68808f802364c15df97f67f1b53b
anindita101 <31119840+anindita101@users.noreply.github.com>
2017-08-21 22:08:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one',function(req,res){
+    res.send('article one requested and will be srved here');
+});
+
+app.get('/article-one',function(req,res){
+    res.send('article one requested and will be srved here');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
